fbff89839dce9fe7216d84a7afa95b42682b7ceb
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-02-10 17:43:00 +0530

    Add files via upload

---
+++ ExplicitCasting.java
@@ -0,0 +1,15 @@
+package logical_programming_DataTypes;
+
+public class ExplicitCasting {
+	public static void main(String[] args) {
+		int i = 100;
+		byte b =(byte)i;
+		int x = 97;
+		char e = 'A';
+		char ch =(char)x;
+		System.out.println(ch);
+
+		
+	}
+	
+}
