8870cb6e1821e14071cc94e018aaa548c5403cf8
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-02-10 17:42:48 +0530

    Add files via upload

---
+++ DataTypesDemo.java
@@ -0,0 +1,26 @@
+package logical_programming_DataTypes;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		
+		byte a = -50;
+		short b = 150;
+		int c = 1000;
+		long d = 1000000;
+		char e = 'A';
+		float f = 20.5f;
+		double g = 12344.54544;
+		byte h =20;
+		int  Result = a+h;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		
+	}
+}
