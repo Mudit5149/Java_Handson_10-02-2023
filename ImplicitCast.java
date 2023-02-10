b918b5bdeb709586867b0057c1740b043ca98c2a
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2023-02-10 17:43:15 +0530

    Add files via upload

---
+++ ImplicitCast.java
@@ -0,0 +1,15 @@
+package logical_programming_DataTypes;
+
+public class ImplicitCast {
+	public static void main(String[] args) {
+		byte b = 100;
+		int i = b;
+		System.out.println(b);
+		long l = i ;
+		char ch ='A';
+
+		
+	}
+	
+
+}
